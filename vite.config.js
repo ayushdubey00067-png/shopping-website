860074f13efae37e4669beb6464f5896da7274e2
Aayush dubey <ayushdubey00067@gmail.com>
2026-02-03 19:23:21 +0530

    add something new

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/shopping-website/', // Aapke repository ka naam yahan likhein
 })
