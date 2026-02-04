5acc87c6dc86bc125929d1cc754508054e9b2c6c
Aayush dubey <ayushdubey00067@gmail.com>
2026-02-04 17:40:36 +0530

    update main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import { CartProvider } from "./context/CartContext";
@@ -7,6 +8,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartProvider>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </CartProvider>
 );
+
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shopping-website/', // Yeh line sabse zaroori hai
+  base: "/shopping-website/"
 })
